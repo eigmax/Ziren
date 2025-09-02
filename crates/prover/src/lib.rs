f3c5f826dfee0b2bf8c6976639f8316e4424fb93
Blake <148928866+blake-pro@users.noreply.github.com>
2025-09-02 08:22:18 +0800

    set default VERIFY_VK true (#295)

--- crates/prover/src/lib.rs
+++ crates/prover/src/lib.rs
@@ -210,7 +210,7 @@ impl<C: ZKMProverComponents> ZKMProver<C> {
             .then_some(RecursionShapeConfig::default());
 
         let vk_verification =
-            env::var("VERIFY_VK").map(|v| v.eq_ignore_ascii_case("true")).unwrap_or(false);
+            env::var("VERIFY_VK").map(|v| v.eq_ignore_ascii_case("true")).unwrap_or(true);
 
         tracing::debug!("vk verification: {}", vk_verification);
 
