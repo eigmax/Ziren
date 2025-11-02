96b0e8610ea39c217494940d552076cc6d108fdd
eigmax <sd@eigen.market>
2025-11-02 10:57:47 +0800

    return post state

--- crates/core/machine/src/shape/mod.rs
+++ crates/core/machine/src/shape/mod.rs
@@ -524,8 +524,8 @@ impl<F: PrimeField32> Default for CoreShapeConfig<F> {
 
 fn derive_cluster_from_maximal_shape(shape: &Shape<MipsAirId>) -> ShapeCluster<MipsAirId> {
     // We first define a heuristic to derive the log heights from the maximal shape.
-    let log2_gap_from_21 = 21 - shape.log2_height(&MipsAirId::Cpu).unwrap();
-    let min_log2_height_threshold = 18 - log2_gap_from_21;
+    let log2_gap_from_22 = 22 - shape.log2_height(&MipsAirId::Cpu).unwrap();
+    let min_log2_height_threshold = 18 - log2_gap_from_22;
     let log2_height_buffer = 10;
     let heuristic = |maximal_log2_height: Option<usize>, min_offset: usize| {
         if let Some(maximal_log2_height) = maximal_log2_height {
